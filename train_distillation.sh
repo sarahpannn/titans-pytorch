bfa5d2d3808827f9bcabb63196da5c8d53f160ba
sarahpannn <sarahpan@mit.edu>
2025-12-08 15:15:28 -0500

    attn distillation

--- train_distillation.sh
+++ train_distillation.sh
@@ -6,21 +6,18 @@ python3 run_training.py \
     --segment_len 512 \
     --sequence_length 2048 \
     --batch_size 32 \
-    --micro_batch_size 1 \
+    --micro_batch_size 4 \
+    --num_persist_mem 0 \
     --neural_memory_segment_len 64 \
-    --neural_memory_batch_size 32 \
-    --total_tokens 10000000 \
+    --neural_memory_batch_size 64 \
+    --total_tokens 1000000000 \
     --eval_interval 500 \
     --save_interval 2000 \
-    --log_interval 50 \
-    --intermittent_eval_frequency 100 \
+    --log_interval 1 \
+    --intermittent_eval_frequency 30 \
     --intermittent_eval_limit 128 \
-    --intermittent_eval_start_step 100 \
+    --intermittent_eval_start_step 10 \
     --use_attention_distillation \
-    --distillation_weight 0.1 \
+    --distillation_weight 0.7 \
     --distillation_layers "4,8,12,16,20" \
-    --neural_memory_layers "4,8,12,16,20" \
-    --no_wandb \
-    --debug
-
-echo "Training completed!"
+    --neural_memory_layers "4,8,12,16,20"
